d2396de0950b3c34fb14b9179ffbaecade5ba031
Sam Ellis <theop8r@proton.me>
2023-05-18 09:42:17 -0400

    Use env config to remove hardcoded values. Also adds general test comment.

--- tests.js
+++ tests.js
@@ -1,24 +1,27 @@
 const axios = require('axios')
 const utils = require('./utils.js')  
 
-const host = 'http://localhost:3000'
+const env = utils.envConfig()
+const host = `http://localhost:${env.PORT}`
+
+/* For these tests, our goal is to simply utilize all the
+ * standard endpoints in a realistic sequence. So, first we
+ * hit first-message, reply to first-message, then new message, 
+ * followed by a reply, and finally- a scheduled message.
+ */
 
 const mockInitialMail = {
     'Message-Id': '<CAH8yDf6h5h7HQ_vx4e@mail.gmail.com>',
-    'recipient': 'peace.pal@mail.riseofwizards.com',
+    'recipient': `${env.APP_ADDRESS}@${env.MAIL_DOMAIN}`,
     'sender': 'sam@wpmc.fund',
     'stripped-text': 'Hi- hows this werkk?',
     'subject': 'trying this out'
 }
 
-const mockInitialReply = {
-    'Message-Id': '<OPR8yDf6h5h7HQ_vx4e@mail.gmail.com>',
-    'In-Reply-To': '<CAH8yDf6h5h7HQ_vx4e@mail.gmail.com>',
-    'recipient': 'peace.pal@mail.riseofwizards.com',
-    'sender': 'sam@wpmc.fund',
-    'stripped-text': 'Uncertainty, Sadness',
-    'subject': 'Re: trying this out'
-}
+let mockInitialReply = mockInitialMail
+mockInitialReply['In-Reply-To'] = '<CAH8yDf6h5h7HQ_vx4e@mail.gmail.com>' 
+mockInitialReply['stripped-text'] = 'Uncertainty, Sadness' 
+mockInitialReply['subject'] = 'Re: trying this out' 
 
 const runTests = async () => {
     const response  = await axios.post(host + '/ingress', mockInitialMail)
