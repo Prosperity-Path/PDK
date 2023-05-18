dc37243bf943ba2ad8650b45ef706c3340605c13
Sam Ellis <theop8r@proton.me>
2023-05-18 10:58:58 -0400

    pull text from triggers json instead of hard-code in mock mail

--- tests.js
+++ tests.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
 const utils = require('./utils.js')  
+const triggers = require('./triggers.json')
 
 const env = utils.envConfig()
 const host = `http://localhost:${env.PORT}`
@@ -10,24 +11,31 @@ const host = `http://localhost:${env.PORT}`
  * followed by a reply, and finally- a scheduled message.
  */
 
+const firstTrigger = triggers.find(t => t.trigger == 'first-message')
 const mockInitialMail = {
     'Message-Id': '<CAH8yDf6h5h7HQ_vx4e@mail.gmail.com>',
     'recipient': `${env.APP_ADDRESS}@${env.MAIL_DOMAIN}`,
     'sender': 'sam@wpmc.fund',
-    'stripped-text': 'Hi- hows this werkk?',
+    'stripped-text': firstTrigger.testInput,
     'subject': 'trying this out'
 }
 
+const replyTrigger = triggers.find(t => t.trigger == 'reply')
 let mockInitialReply = mockInitialMail
 mockInitialReply['In-Reply-To'] = '<CAH8yDf6h5h7HQ_vx4e@mail.gmail.com>' 
-mockInitialReply['stripped-text'] = 'Uncertainty, Sadness' 
+mockInitialReply['stripped-text'] = replyTrigger.testInput 
 mockInitialReply['subject'] = 'Re: trying this out' 
 
+const newTrigger = triggers.find(t => t.trigger == 'new-message')
+let mockNewMail = mockInitialMail
+mockNewMail['subject'] = 'New thought'
+mockNewMail['stripped-text'] = newTrigger.testInput
+
 const runTests = async () => {
-    const response  = await axios.post(host + '/ingress', mockInitialMail)
+    const response = await axios.post(host + '/ingress', mockInitialMail)
     console.log(response.data)
 
-    const replyRes  = await axios.post(host + '/ingress', mockInitialReply)
+    const replyRes = await axios.post(host + '/ingress', mockInitialReply)
     console.log(replyRes)
 }
 
