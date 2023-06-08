ee6aed08f7e9110b90b19b8d912724cd51847b1e
Sam Ellis <sam.dave.ellis@gmail.com>
2023-06-08 09:36:21 -0400

    Add default scheduling poll for health check

--- utils/defaults.js
+++ utils/defaults.js
@@ -2,5 +2,6 @@ module.exports = {
     CMD_CTRL:'http://slash.athari.app',
     MAIL_DOMAIN:'http://dev.athari.app',
     APP_TARGET:'http://localhost:8080',
-    PORT:3000
+    PORT:3000,
+    POLL_INTERVAL:30000
 }
